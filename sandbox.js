79764c7eb39a5a92d6797c60a81da8b32973ed45
PnTsoi <pntsoi@ucsd.edu>
2022-09-16 16:15:29 -0700

    Add some stuffs

--- sandbox.js
+++ sandbox.js
@@ -6,3 +6,21 @@ const nodeListCards = bookContainer.querySelectorAll(".card");
 
 console.log(nodeListCards);
 
+const side = mainContainer.querySelector("#side");
+const statList = side.querySelector("ul.stat-list");
+const createBookButton = statList.querySelector("button");
+console.log(createBookButton);
+createBookButton.addEventListener("click", createBook);
+
+function bookObj(bookName, bookAuthor, bookPage, pageRead, bookStatus) {
+    this.bookName = bookName;
+    this.bookAuthor = bookAuthor;
+    this.bookPage = bookPage;
+    this.pageRead = pageRead;
+    this.bookStatus = bookStatus;
+}
+
+function createBook() {
+     
+}
+

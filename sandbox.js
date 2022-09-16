ddd89ba1efd8a930c4018b508243d8b9c3cfe8ee
PnTsoi <pntsoi@ucsd.edu>
2022-09-16 15:01:58 -0700

    Change flexbox sizing

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+const mainContainer = document.querySelector(".main-container");
+const main = mainContainer.querySelector("#main");
+const bookContainer = main.querySelector(".container");
+
+const nodeListCards = bookContainer.querySelectorAll(".card");
+
+console.log(nodeListCards);
+
